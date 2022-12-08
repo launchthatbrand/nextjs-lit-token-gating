3db67c3f115fa216caeef3006579f7dfaedb33ba
Desmond Tatilian <info@launchthatbrand.com>
2022-12-08 12:46:03 -0500

    added landing page without tailwind css styling

---
+++ components/Dropdowns/UserDropdown.js
@@ -0,0 +1,92 @@
+import React from "react";
+import Link from "next/link";
+import { createPopper } from "@popperjs/core";
+import { useSession, signOut } from "next-auth/react";
+
+const UserDropdown = () => {
+  const { data: session, status } = useSession();
+  // dropdown props
+  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+  const btnDropdownRef = React.createRef();
+  const popoverDropdownRef = React.createRef();
+  const openDropdownPopover = () => {
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: "bottom-start",
+    });
+    setDropdownPopoverShow(true);
+  };
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+
+  return (
+    <>
+      <a
+        className="text-blueGray-500 block"
+        href="#pablo"
+        ref={btnDropdownRef}
+        onClick={(e) => {
+          e.preventDefault();
+          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+        }}
+      >
+        <div className="items-center flex">
+          <span className="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full">
+            <img
+              alt="..."
+              className="w-full rounded-full align-middle border-none shadow-lg"
+              src={session ? session?.user?.image : "/img/team-1-800x800.jpg"}
+            />
+          </span>
+        </div>
+      </a>
+      <div
+        ref={popoverDropdownRef}
+        className={
+          (dropdownPopoverShow ? "block " : "hidden ") +
+          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+        }
+      >
+        <a
+          href="/admin/settings"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          }
+          // onClick={(e) => e.preventDefault()}
+        >
+          {session?.user?.email}
+        </a>
+        <a
+          href="#pablo"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+          Another action
+        </a>
+        <a
+          href="#pablo"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+          Something else here
+        </a>
+        <div className="h-0 my-2 border border-solid border-blueGray-100" />
+        <a
+          href="#pablo"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          }
+          onClick={(e) => signOut()}
+        >
+          Sign Out
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default UserDropdown;
